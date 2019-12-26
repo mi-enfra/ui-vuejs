b516952d4ea5ac4b2104e61ddf20f6656f99e081
Marvin Isaac <marc.cr51rep@gmail.com>
2019-12-26 18:40:51 +0000

    :construction: Setup sample development environment
    TODO Transform bash scripts to docker-compose

--- dev-start.sh
+++ dev-start.sh
@@ -1,5 +1,14 @@
+#! /bin/bash
+
+DIR=$PWD"/dist"
+
+docker run -d --rm \
+    -e VIRTUAL_HOST=ui-vuejs.docker-experiment.com \
+    -v ${DIR}:/usr/share/nginx/html \
+    --name ui-vuejs \
+    nginx:alpine
+
 docker run -it --rm \
     -v ${PWD}:/var/www/html/app \
-    --name ui-vuejs \
-    --env VIRTUAL_HOST=ui-vuejs.docker-experiment.com \
+    --name ui-vuejs-dev \
     marvinenf/vuejs-dev
